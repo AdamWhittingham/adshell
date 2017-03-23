788b37b658ec41afb4dce27403058756fe6d63d8
Duncan Fermor <duncan_fermor@fastmail.com>
2017-03-23 15:00:36 +0000

    untested - search and replace on 'type'

--- zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+++ zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
@@ -183,8 +183,8 @@ _zsh_highlight_load_highlighters()
     highlighter="${highlighter_dir:t}"
     [[ -f "$highlighter_dir/${highlighter}-highlighter.zsh" ]] && {
       . "$highlighter_dir/${highlighter}-highlighter.zsh"
-      type "_zsh_highlight_${highlighter}_highlighter" &> /dev/null &&
-      type "_zsh_highlight_${highlighter}_highlighter_predicate" &> /dev/null || {
+      command -v "_zsh_highlight_${highlighter}_highlighter" &> /dev/null &&
+      command -v "_zsh_highlight_${highlighter}_highlighter_predicate" &> /dev/null || {
         echo "zsh-syntax-highlighting: '${highlighter}' highlighter should define both required functions '_zsh_highlight_${highlighter}_highlighter' and '_zsh_highlight_${highlighter}_highlighter_predicate' in '${highlighter_dir}/${highlighter}-highlighter.zsh'." >&2
       }
     }
