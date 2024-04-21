c70cdf7315c016e277db944dbe9f154b49a37488
Ad Whittingham <adam.whittingham@gmail.com>
2024-04-21 17:22:58 +0100

    Stub ruby prompt

---
+++ prompt-rb/lib/color.rb
@@ -0,0 +1,57 @@
+class Color
+  ANSI_COLORS = 16
+
+  def self.fg(r,g,b, output: :_24bit)
+    case output
+    when :_24bit
+      new(r,g,b).to_24bit
+    when :_256
+      new(r,g,b).to_256
+    end
+  end
+
+  def self.bg(r,g,b, output: :_24bit)
+    case output
+    when :_24bit
+      new(r,g,b).to_24bit(mode: :bg)
+    when :_256
+      new(r,g,b).to_256(mode: :bg)
+    end
+  end
+
+  def self.reset
+    "\e[0m"
+  end
+
+  def initialize(r, g, b)
+    @r = r
+    @g = g
+    @b = b
+  end
+
+  def mode(mode)
+    case mode
+    when :fg then 38
+    when :bg then 48
+    end
+  end
+
+  # https://tintin.mudhalla.net/info/256color/
+  def to_256(mode: :fg)
+    # color = (r*6/256)*36 + (g*6/256)*6 + (b*6/256)
+    code = ANSI_COLORS +
+      (36 * ansi_weight(@r).to_i) +
+      (6 * ansi_weight(@g).to_i) +
+      (1 * ansi_weight(@b).to_i)
+
+    "\e[#{mode(mode)};5;#{code}m"
+  end
+
+  private def ansi_weight(value)
+    (5 * value) / 255.0
+  end
+
+  def to_24bit(mode: :fg)
+    "\e[#{mode(mode)};2;#{@r};#{@g};#{@b}m"
+  end
+end
---
+++ prompt-rb/spec/color_spec.rb
@@ -0,0 +1,43 @@
+require_relative "../lib/color.rb"
+
+describe Color do
+  describe "#to_256" do
+    it "can convert RGB to 256 color" do
+      expect(described_class.new(0, 0, 0).to_256).to eq "\e[38;5;16m"
+
+      expect(described_class.new(255, 0, 0).to_256).to eq "\e[38;5;196m"
+      expect(described_class.new(0, 255, 0).to_256).to eq "\e[38;5;46m"
+      expect(described_class.new(0, 0, 255).to_256).to eq "\e[38;5;21m"
+
+      expect(described_class.new(255, 255, 0).to_256).to eq "\e[38;5;226m"
+      expect(described_class.new(0, 255, 255).to_256).to eq "\e[38;5;51m"
+      expect(described_class.new(255, 255, 255).to_256).to eq "\e[38;5;231m"
+
+      # Spring Green 3
+      expect(described_class.new(0, 175, 95).to_256).to eq "\e[38;5;35m"
+
+      # Cadet Blue
+      expect(described_class.new(95, 175, 175).to_256).to eq "\e[38;5;73m"
+    end
+  end
+
+  describe "#to_24bit" do
+    it "ouputs 24bit color" do
+      expect(described_class.new(0, 0, 0).to_24bit).to eq "\e[38;2;0;0;0m"
+
+      expect(described_class.new(255, 0, 0).to_24bit).to eq "\e[38;2;255;0;0m"
+      expect(described_class.new(0, 255, 0).to_24bit).to eq "\e[38;2;0;255;0m"
+      expect(described_class.new(0, 0, 255).to_24bit).to eq "\e[38;2;0;0;255m"
+
+      expect(described_class.new(255, 255, 0).to_24bit).to eq "\e[38;2;255;255;0m"
+      expect(described_class.new(0, 255, 255).to_24bit).to eq "\e[38;2;0;255;255m"
+      expect(described_class.new(255, 255, 255).to_24bit).to eq "\e[38;2;255;255;255m"
+
+      # Spring Green 3
+      expect(described_class.new(0, 175, 95).to_24bit).to eq "\e[38;2;0;175;95m"
+
+      # Cadet Blue
+      expect(described_class.new(95, 175, 175).to_24bit).to eq "\e[38;2;95;175;175m"
+    end
+  end
+end
